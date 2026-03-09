165d3f56f7c066db1483b4b0d589c3e34f3ff044
Jeppe Holm Sørensen <64833246+Jenspeterdumpap@users.noreply.github.com>
2026-03-09 14:33:00 +0100

    working generator for line

--- main/createlineBMSSP.py
+++ main/createlineBMSSP.py
@@ -24,7 +24,7 @@ def create_line_bmssp(budget, memory, target, size, threshold, det, pre):
     sensor_states = [] 
     for s in range(size):
         if s != target:
-            sensor_vars.append(f'ys{s} = Real(\'ys{s}\')')
+            sensor_vars.append(f'y{s} = Real(\'y{s}\')')
             sensor_states.append(s)
     file.write('\n'.join(sensor_vars) + '\n')
 
@@ -53,13 +53,13 @@ def create_line_bmssp(budget, memory, target, size, threshold, det, pre):
         for m in range(1,memory+1):
             for act in actions:
                 for n in range(1,memory+1):
-                    delta_vars.append(f'd_c{m}_o{s}_{act}_c{n} = Real(\'d_c{m}_o{s}_{act}_c{n}\')')
+                    delta_vars.append(f'x_c{m}_o{s}_{act}_c{n} = Real(\'x_c{m}_o{s}_{act}_c{n}\')')
     
     #generate default delta functions (when no sensor observes - unknown state)
     for m in range(1,memory+1):
         for act in actions:
             for n in range(1,memory+1):
-                delta_vars.append(f'd_c{m}_o_{act}_c{n} = Real(\'d_c{m}_o_{act}_c{n}\')')
+                delta_vars.append(f'x_c{m}_o_{act}_c{n} = Real(\'x_c{m}_o_{act}_c{n}\')')
     
 
     file.write('\n'.join(delta_vars) + '\n')
@@ -87,48 +87,54 @@ def create_line_bmssp(budget, memory, target, size, threshold, det, pre):
 
                 equation = f'pi_s{s}_c{m} == '
                 for act in actions:
-                    for n in range(memory):
-                        if act != 'l':
-                            equation += "+ "
-                        elif n != 1:
-                            equation += "+ "
-                        equation += f'(1 + pi_s{left_next}_c{n}) * (y{s} * x_c{m}_o{s}_{act} * x_c{m}_o{s}_{act}_c{n} + (1 - ys{s}) * x_c{m}_o_{act} * x_c{m}_o_{act}_c{n}) \n'
+                    for n in range(1, memory+1):
+                        if act == actions[0] and n == 1:
+                            equation += f'(1 + pi_s{left_next}_c{n}) * (y{s} * x_c{m}_o{s}_{act} * x_c{m}_o{s}_{act}_c{n} + (1 - y{s}) * x_c{m}_o_{act} * x_c{m}_o_{act}_c{n}) \n'
+                        else:
+                            equation += f'+ (1 + pi_s{left_next}_c{n}) * (y{s} * x_c{m}_o{s}_{act} * x_c{m}_o{s}_{act}_c{n} + (1 - y{s}) * x_c{m}_o_{act} * x_c{m}_o_{act}_c{n}) \n'
                 file.write(equation + ',\n')
     
     file.write(f'# We are dropped uniformly in the line\n# We want to check if the minimal expected cost is below some threshold {threshold}\n')
     pi_terms = [f'pi_s{s}_c1' for s in range(size) if s != target]
     pi_sum = '+'.join(pi_terms)
-    threshold_constraint = f'({pi_sum}) * Q(1,{size-1}) {threshold},'
+    threshold_constraint = f'({pi_sum}) * Q(1,{size-1}) <= {threshold},'
 
     #may need later
     e = f'({pi_sum}) * Q(1,{size-1}) '
 
     file.write(threshold_constraint + '\n')
 
-    file.write('#randomized strategy theta')
+    file.write('#randomized strategy theta \n')
     theta_strategy_constraints = []
     for s in sensor_states:
         for m in range(1,memory+1):
+            thetaSum = ""
             for act in actions:
                 theta_strategy_constraints.append(f'x_c{m}_o{s}_{act} >= 0')
                 theta_strategy_constraints.append(f'x_c{m}_o{s}_{act} <= 1')
-                theta_strategy_constraints.append(f'x_c{m}_o{s}_{act} == 0 or x_c{m}_o{s}_{act} == 1')
+                thetaSum += f'x_c{m}_o{s}_{act} + '
+            thetaSum = thetaSum[:-3]
+            theta_strategy_constraints.append(f'{thetaSum} == 1')
 
     #add defult strategy constraints
     for m in range(1,memory+1):
+        thetaSum = ""
         for act in actions:
-            theta_strategy_constraints.append(f'x_c{m}_o_{act} >= 0')
-            theta_strategy_constraints.append(f'x_c{m}_o_{act} <= 1')
-            theta_strategy_constraints.append(f'x_c{m}_o_{act} == 0 or x_c{m}_o_{act} == 1')       
+            theta_strategy_constraints.append(f'x_c{m}_o{s}_{act} >= 0')
+            theta_strategy_constraints.append(f'x_c{m}_o{s}_{act} <= 1')
+            thetaSum += f'x_c{m}_o{s}_{act} + '
+        thetaSum = thetaSum[:-3]
+        theta_strategy_constraints.append(f'{thetaSum} == 1')      
                             
-    file.write(',\n'.join(theta_strategy_constraints) + '\n')
+    file.write(',\n'.join(theta_strategy_constraints) + ',\n')
 
     delta_strategy_constraints = []
-    for s in sensor_states:
-        for m in range(1,memory+1):
-            for act in actions:
+    
+    for m in range(1,memory+1):
+        for s in sensor_states:
+            for n in range(1,memory+1):
                 deltaSum = ""
-                for n in range(1,memory+1):
+                for act in actions:
                     delta_strategy_constraints.append(f'x_c{m}_o{s}_{act}_c{n} >= 0')
                     delta_strategy_constraints.append(f'x_c{m}_o{s}_{act}_c{n} <= 1')
                     deltaSum += f'x_c{m}_o{s}_{act}_c{n} + '
@@ -137,21 +143,21 @@ def create_line_bmssp(budget, memory, target, size, threshold, det, pre):
 
     #add defult delta strategy constraints
     for m in range(1,memory+1):
-        for act in actions:
+        for n in range(1,memory+1):
             deltaSum = ""
-            for n in range(1,memory+1):
+            for act in actions:
                 delta_strategy_constraints.append(f'x_c{m}_o_{act}_c{n} >= 0')
                 delta_strategy_constraints.append(f'x_c{m}_o_{act}_c{n} <= 1')
                 deltaSum += f'x_c{m}_o_{act}_c{n} + '
             deltaSum = deltaSum[:-3]
-            delta_strategy_constraints.append(f'{deltaSum} == 1')
+            delta_strategy_constraints.append(f'{deltaSum} == 1') 
     
-    file.write(',\n'.join(delta_strategy_constraints) + '\n')
+    file.write(',\n'.join(delta_strategy_constraints) + ',\n')
     
     #needs to add section on deterministic strategies if det == 1
 
     file.write('# y is a function that should map every state N to some observable class M\n')
-    sensor_binary_constraints = [f'Or(ys{s} == 0, ys{s} == 1),' for s in sensor_states]
+    sensor_binary_constraints = [f'Or (y{s} == 0, y{s} == 1),' for s in sensor_states]
     file.write('\n'.join(sensor_binary_constraints) + '\n') 
     sensor_sum = ' + '.join([f'y{s}' for s in sensor_states])    
     file.write(f'{sensor_sum} == {budget}')           
