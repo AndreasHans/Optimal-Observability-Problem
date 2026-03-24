f4d4294d5c2f043634ef1c768c1d5d5917cd16db
Jeppe Holm Sørensen <64833246+Jenspeterdumpap@users.noreply.github.com>
2026-03-24 15:58:08 +0100

    maze part 1

--- main/MDPVariants.py
+++ main/MDPVariants.py
@@ -5,13 +5,13 @@ def grid_center(n:int, m:int):
         raise ValueError("n and m must be odd")
     print(f"Creating grid MDP with {n} rows and {m} columns")
 
-    mdp = MDP(_states=n*m, _initial_states=[(j*n + i) for i in range(n) for j in range(m) if (i != n//2 and j != m//2)], _goals=[(m//2) * n + (n//2)], _actions=["left", "right", "up", "down"])
+    mdp = MDP(_states=n*m, _initial_states=[i for i in range(n*m) if (i != (n*m)//2)], _goals=[(m//2) * n + (n//2)], _actions=["left", "right", "up", "down"])
     # make transitions
     for s in mdp.states():
-        if s-1 % n == 0:
+        if s % n == 0: 
             mdp.set_transition(s, "right", s+1, 1.0)
             mdp.set_transition(s, "left", s, 1.0)
-        elif s-(n-1) % n == 0:
+        elif s-(n-1) % n == 0: #works
             mdp.set_transition(s, "right", s, 1.0)
             mdp.set_transition(s, "left", s-1, 1.0)
         else:
@@ -42,8 +42,85 @@ def grid_center(n:int, m:int):
         mdp.set_optimal_cost(s, abs(row - m//2) + abs(col - n//2))
     
     return mdp
+
+
+def maze(columns:int, rows:int):
+    if columns % 2 == 0:
+        raise ValueError("columns must be odd")
+    print(f"Creating maze MDP with {columns} columns and {rows} rows")
+    NumStates = columns+3*(rows -1)
+    mdp = MDP(_states=NumStates, _initial_states=[i for i in range(NumStates) if (i != NumStates - rows)], _goals=[NumStates - rows], _actions=["left", "right", "up", "down"])
+
+    for s in mdp.states():
+        #right/left 
+        if s >= columns:
+            mdp.set_transition(s, "left", s, 1.0)
+            mdp.set_transition(s, "right", s, 1.0)
+        elif s == 0:
+            mdp.set_transition(s, "left", s, 1.0)
+            mdp.set_transition(s, "right", s+1, 1.0)
+        elif s == columns -1:
+            mdp.set_transition(s, "left", s-1, 1.0)
+            mdp.set_transition(s, "right", s, 1.0)
+        else:
+            mdp.set_transition(s, "left", s-1, 1.0)
+            mdp.set_transition(s, "right", s+1, 1.0)
+
+        #up 
+        #the line
+        if s < columns: 
+             mdp.set_transition(s, "up", s, 1.0)
+        #the part of the leg that goes to the line 
+        elif s-columns >= 0 and (s-columns)%3 == 0:
+            mdp.set_transition(s, "up", s-columns, 1.0)
+        else:
+            mdp.set_transition(s, "up", s-1 , 1.0)
+
+        #down 
+        #first 3: the case from going down from the line
+        if s == 0:
+            mdp.set_transition(s, "down", columns, 1.0)
+        elif s ==  (columns-1)/2:
+            mdp.set_transition(s, "down", (columns + rows - 1), 1.0)
+        elif s == columns-1:
+            mdp.set_transition(s, "down", (2*(rows - 1) + columns), 1.0)
+        #the rest of the line
+        elif s < columns:
+            mdp.set_transition(s, "down", s, 1.0)
+        # the bottom three
+        elif (s - (columns - 1)) % (rows -1) == 0:
+            mdp.set_transition(s, "down", s, 1.0)
+        else:
+            mdp.set_transition(s, "down", s+1, 1.0)
+        
+    #set reward
+    for s in mdp.states():
+        mdp.set_reward(s, 1.0)
     
-     
+    for s in mdp.goals():
+        mdp.set_reward(s, 0)
+
+    #set minimum cost
+    mid = (columns-1)/2 
+    goal = NumStates - rows
+    for s in mdp.states():
+        
+        if s < columns:
+            #the line
+            mdp.set_optimal_cost(s, abs(s-mid) + rows-1)
+        elif s < (columns + rows - 1):
+            mdp.set_optimal_cost(s, s- (columns -1) + mid + (rows - 1))
+        elif s < (columns + 2* (rows -1)):
+            mdp.set_optimal_cost(s, goal- s)  
+        else:
+            mdp.set_optimal_cost(s, s- goal + mid + rows - 1)     
+   
+   
+    return mdp
+
+            
+        
+    
 
 def line_n(n:int):
     if n % 2 == 0:
