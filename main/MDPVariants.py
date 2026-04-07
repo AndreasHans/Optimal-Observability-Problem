ab57467d477de1a039517047c899982a2561ac41
Andreas Hansen <andreas.hansen02@hotmail.com>
2026-04-07 11:36:28 +0200

    fix maze n

--- main/MDPVariants.py
+++ main/MDPVariants.py
@@ -44,57 +44,63 @@ def grid_center(n:int, m:int):
     return mdp
 
 def maze_n(n:int):
-    return maze(n, ((n-1)/2))
+    return maze(n, ((n-1)//2))
 
 def maze(columns:int, rows:int):
     if columns % 2 == 0:
         raise ValueError("columns must be odd")
     print(f"Creating maze MDP with {columns} columns and {rows} rows")
-    NumStates = columns+3*(rows -1)
-    mdp = MDP(_states=NumStates, _initial_states=[i for i in range(NumStates) if (i != NumStates - rows)], _goals=[NumStates - rows], _actions=["left", "right", "up", "down"])
+    num_states = columns+3*rows
+    goal = num_states - rows - 1
+    mid = (columns - 1) // 2
 
-    for s in mdp.states():
-        #right/left 
-        if s >= columns:
+    mdp = MDP(_states=num_states, _initial_states=[s for s in range(num_states) if (s != goal)], _goals=[goal], _actions=["left", "right", "up", "down"])
+
+    def set_vertical_leg_transitions(start:int, up_target:int):
+        end = start + rows
+        for s in range(start, end):
+            if s == start:
+                mdp.set_transition(s, "up", up_target, 1.0)
+                mdp.set_transition(s, "down", s+1, 1.0)
+            elif s == end - 1:
+                mdp.set_transition(s, "up", s-1, 1.0)
+                mdp.set_transition(s, "down", s, 1.0)
+            else:
+                mdp.set_transition(s, "up", s-1, 1.0)
+                mdp.set_transition(s, "down", s+1, 1.0)
+
             mdp.set_transition(s, "left", s, 1.0)
             mdp.set_transition(s, "right", s, 1.0)
-        elif s == 0:
-            mdp.set_transition(s, "left", s, 1.0)
-            mdp.set_transition(s, "right", s+1, 1.0)
-        elif s == columns -1:
-            mdp.set_transition(s, "left", s-1, 1.0)
-            mdp.set_transition(s, "right", s, 1.0)
-        else:
-            mdp.set_transition(s, "left", s-1, 1.0)
-            mdp.set_transition(s, "right", s+1, 1.0)
 
-        #up 
-        #the line
-        if s < columns: 
-             mdp.set_transition(s, "up", s, 1.0)
-        #the part of the leg that goes to the line 
-        elif s-columns >= 0 and (s-columns)%(rows-1) == 0:
-            mdp.set_transition(s, "up", s-columns, 1.0)
-        else:
-            mdp.set_transition(s, "up", s-1 , 1.0)
-
-        #down 
-        #first 3: the case from going down from the line
+    # All transitions for the line part
+    for s in range(columns):
         if s == 0:
+            mdp.set_transition(s, "right", s+1, 1.0)
+            mdp.set_transition(s, "left", s, 1.0)
             mdp.set_transition(s, "down", columns, 1.0)
-        elif s ==  (columns-1)/2:
-            mdp.set_transition(s, "down", (columns + rows - 1), 1.0)
+            mdp.set_transition(s, "up", s, 1.0)
+        elif s == mid:
+            mdp.set_transition(s, "right", s+1, 1.0)
+            mdp.set_transition(s, "left", s-1, 1.0)
+            mdp.set_transition(s, "down", columns + rows, 1.0)
+            mdp.set_transition(s, "up", s, 1.0)
         elif s == columns-1:
-            mdp.set_transition(s, "down", (2*(rows - 1) + columns), 1.0)
-        #the rest of the line
-        elif s < columns:
-            mdp.set_transition(s, "down", s, 1.0)
-        # the bottom three
-        elif (s - (columns - 1)) % (rows -1) == 0:
-            mdp.set_transition(s, "down", s, 1.0)
+            mdp.set_transition(s, "right", s, 1.0)
+            mdp.set_transition(s, "left", s-1, 1.0)
+            mdp.set_transition(s, "down", (2* rows + columns), 1.0)
+            mdp.set_transition(s, "up", s, 1.0)
         else:
-            mdp.set_transition(s, "down", s+1, 1.0)
-        
+            mdp.set_transition(s, "right", s+1, 1.0)
+            mdp.set_transition(s, "left", s-1, 1.0)
+            mdp.set_transition(s, "down", s, 1.0)
+            mdp.set_transition(s, "up", s, 1.0)
+
+    set_vertical_leg_transitions(columns, 0)
+    set_vertical_leg_transitions(columns + rows, mid)
+    set_vertical_leg_transitions(columns + 2 * rows, columns-1)
+
+
+                
     #set reward
     for s in mdp.states():
         mdp.set_reward(s, 1.0)
@@ -103,19 +109,18 @@ def maze(columns:int, rows:int):
         mdp.set_reward(s, 0)
 
     #set minimum cost
-    mid = (columns-1)/2 
-    goal = NumStates - rows
+    mid = columns//2 
     for s in mdp.states():
-        
+
         if s < columns:
             #the line
-            mdp.set_optimal_cost(s, abs(s-mid) + rows-1)
-        elif s < (columns + rows - 1):
-            mdp.set_optimal_cost(s, s- (columns -1) + mid + (rows - 1))
-        elif s < (columns + 2* (rows -1)):
-            mdp.set_optimal_cost(s, goal- s)  
+            mdp.set_optimal_cost(s, abs(s-mid) + rows)
+        elif s < columns + rows:
+            mdp.set_optimal_cost(s, mid + rows + s - columns + 1)
+        elif s < (columns + 2 * rows):
+            mdp.set_optimal_cost(s, goal - s)  
         else:
-            mdp.set_optimal_cost(s, s- goal + mid + rows - 1)     
+            mdp.set_optimal_cost(s, mid + rows + s - (columns + 2 * rows) + 1)  
    
    
     return mdp
